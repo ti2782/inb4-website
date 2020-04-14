052fe56419bdd611a2ebd7c1b960520288578975
ti2782 <ti2782@protonmail.com>
2020-04-14 09:35:43 -0400

    Add markers

--- markers/yourmaster/index.php
+++ markers/yourmaster/index.php
@@ -3,10 +3,10 @@
   <head>    
     <!--  Meta  -->
     <meta charset="UTF-8" />
-    <title>INB4SAUCE</title>
+    <title>[YOUR] MASTER</title>
 
     <!--  Styles  -->
-    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="../../styles/main.css">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,7 +15,6 @@
       <!-- Navbar content -->
       <a class="navbar-brand" href='https://inb4sauce.net'>INB4SAUCE</a>
       <form class="form-inline">
-        <a class="navbar-brand" href='https://inb4sauce.net/posts.php'>POSTS</a>
         <a class="navbar-brand" href='https://inb4sauce.net/markers.php'>MARKERS</a>
 	<input class="form-control mr-sm-2" type="search" id="search-text" placeholder="Search" aria-label="Search">
 	<button class="btn btn-outline-success my-2 my-sm-0" id="search-button" type="submit" onClick="javascript:searchButton()">Search</button>
@@ -25,12 +24,12 @@
   <body>
     
     <div class="alert alert-dark" role="alert">
-      UNDER MAINTENANCE. NEVER WRONG. Ahead of schedule. Below budget. Anon delivers. Always.
+    Satan laughing spreads his wings.
     </div>
 
     <!-- INCLUDE PAGE -->
     <?php
-     require 'utils.php';
+     require '../../utils.php';
      $lastpage = file_get_contents("last_page.txt");
 
      if(isset($_GET['pages'])) {
@@ -47,6 +46,6 @@
     getFooter();
     ?>
     <!-- Scripts -->
-    <script src="scripts/search.js"></script> 
+    <script src="../../scripts/search.js"></script> 
   </body>
 </html>

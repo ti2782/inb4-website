993697153b285284b36fa6896be111365f4412d2
ti2782 <ti2782@protonmail.com>
2020-10-13 04:06:14 -0400

    Add new Marker WHITERTHANYOU

---
+++ markers/whiterthanyou/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+   <?php
+      require(__DIR__ . '/../../utils.php');
+      include("../../markers-header.html");
+?>
+  <body>
+    
+    <div class="alert alert-dark" role="alert">
+     SO WHITE, YOU CAN'T SEE ME. SO WHITE, I AM TRANSLUCENT.
+    </div>
+
+    <!-- INCLUDE PAGE -->
+    <?php
+     $lastpage = file_get_contents("html/last_page.txt");
+
+     if(isset($_GET['pages'])) {
+     $page = $_GET['pages'];
+     include("html/$page.html");
+     echo "<br><br>";
+    getPages($lastpage, $page);
+    }
+    else {
+    include("html/$lastpage.html");
+    echo "<br><br>";
+    getPages($lastpage, $lastpage);
+    }
+    getFooter();
+    ?>
+    <!-- Scripts -->
+    <script src="../../scripts/search.js"></script> 
+  </body>
+</html>

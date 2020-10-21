ab85597e12aad7ebd4e16c0790e3641cfd5ba5e7
ti2782 <ti2782@protonmail.com>
2020-10-21 05:54:31 -0400

    Add Date Range Search

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,6 @@
+var dateStart;
+var dateEnd;
+
 function searchButton() {
     var search = document.getElementById('search-text').value;
     if(search) {
@@ -7,7 +10,16 @@ function searchButton() {
 	else {
 	    var bexact = exact.checked;
 	    if(bexact) {
-		open("search.php?" + "search=" + encodeURIComponent(search) + "&exact=true");
+		if(dateStart && dateEnd)
+		{
+		    open("search.php?" + "search=" + encodeURIComponent(search) + "&exact=true"
+			 + "&dateStart=" + dateStart
+			 + "&dateEnd=" + dateEnd);
+		}
+		else
+		{
+		    open("search.php?" + "search=" + encodeURIComponent(search) + "&exact=true");
+		}
 		//open(encodeURIComponent("search2.php?search=" + search + "&exact=true"));
 	    }
 	    else {	    
@@ -17,3 +29,12 @@ function searchButton() {
 	}
     }
 }
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+	opens: 'center'
+    }, function(start, end, label) {
+	dateStart = start.format('YYYY-MM-DD');
+	dateEnd = end.format('YYYY-MM-DD');
+    });
+});
--- search.php
+++ search.php
@@ -20,7 +20,7 @@
         <a class="navbar-brand" href='https://inb4sauce.net/markers.php'>MARKERS</a>
 	<input class="form-control mr-sm-2" type="search" id="search-text" placeholder="Search" aria-label="Search">
   <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exactCheck">
+    <input type="checkbox" class="form-check-input" id="exactCheck" checked>
     <label class="form-check-label" for="exactCheck">Exact Match</label>
   </div>
     &nbsp;  
@@ -33,7 +33,23 @@
     <div class="alert alert-dark" role="alert">
     	 NEVER WRONG. Ahead of schedule. Below budget. Anon delivers. Always.
     </div>
-
+    <div class="d-flex justify-content-center">
+      <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+         Advanced Search
+      </a>
+    </div>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+                              <center>DATE RANGE</center>
+          <div class="d-flex justify-content-center">
+            <div class="md-form">
+               <input placeholder="Selected Date Range" type="text" name="daterange" value="01/01/2019 - 01/01/2020" class="form-control datepicker">
+            </div>           
+          </div>
+        </div>
+      </div>
+    <br>
+         
     <!-- MONGODB -->
     <?php
      if(isset($_GET['search'])) {
@@ -46,21 +62,40 @@
          $exact = true;
      else
          $exact = false;
+     if(isset($_GET['dateStart']))
+         $dateStart = $_GET['dateStart'];
+     if(isset($_GET['dateEnd']))
+         $dateEnd = $_GET['dateEnd'];
 
 	require 'vendor/autoload.php';
 
     
 	$coll = (new MongoDB\Client())->inb4->posts;
 	$threadcoll = (new MongoDB\Client())->inb4->threads;
+
+    $sort = array('sort' => array('timestamp'=> -1));
+    
     if($exact) {
-        $cursor = $coll->find(array('$text' => array('$search' => '"'.$search.'"')));    
-        $pageNum = ceil(iterator_count($cursor) / 32);
-        $cursor = $coll->find(array('$text' => array('$search' => '"'.$search.'"')), array('sort' => array('timestamp'=>-1)));
+        if($dateStart && $dateEnd) {
+            $start = new DateTime($dateStart);
+            $end = new DateTime($dateEnd);
+
+            $query = array('$text' => array('$search' => '"'.$search.'"'), 'timestamp' => array('$gte' => $start->getTimestamp(), '$lt' => $end->getTimestamp()));
+
+        }
+        else {
+            $query = array('$text' => array('$search' => '"'.$search.'"'));
+        }
+        
+        $cursor = $coll->find($query);
+        $pageNum = ceil(iterator_count($cursor) / 32);        
+        $cursor = $coll->find($query, $sort);
     }
-    else {        
-        $cursor = $coll->find(array('$text' => array('$search' => $search)));
+    else {
+        $query = array('$text' => array('$search' => $search));
+        $cursor = $coll->find($query);
         $pageNum = ceil(iterator_count($cursor) / 32);
-        $cursor = $coll->find(array('$text' => array('$search' => $search)));
+        $cursor = $coll->find($query);
     }
     $iterator = new IteratorIterator($cursor);
     $iterator->rewind();
@@ -121,9 +156,15 @@
                <ul class="pagination justify-content-center">';
 
         if($exact) {
-            echo '<li class="page-item">
-                <a class="page-link" href="?page=1&search='.$search.'&exact=true">First</a></li>';                
+            if($dateStart && $dateEnd) {
+                echo '<li class="page-item">
+                <a class="page-link" href="?page=1&search='.$search.'&exact=true&dateStart='.$dateStart.'&dateEnd='.$dateEnd.'">First</a></li>';
             }
+            else {            
+                echo '<li class="page-item">
+                <a class="page-link" href="?page=1&search='.$search.'&exact=true">First</a></li>';
+            }
+        }
         else {            
             echo '<li class="page-item">
                 <a class="page-link" href="?page=1&search='.$search.'">First</a></li>';
@@ -134,7 +175,12 @@
         $nextpage = $page + 1;
         if($prevpage >= 1) {
            if($exact) {
-               echo '<li class="page-item"><a class="page-link" href="?page='.$prevpage.'&search='.$search.'&exact=true">Previous</a></li>';
+               if($dateStart && $dateEnd) {
+                   echo '<li class="page-item"><a class="page-link" href="?page='.$prevpage.'&search='.$search.'&exact=true&dateStart='.$dateStart.'&dateEnd='.$dateEnd.'">Previous</a></li>';
+               }
+               else
+                   echo '<li class="page-item"><a class="page-link" href="?page='.$prevpage.'&search='.$search.'&exact=true">Previous</a></li>';
+
            }
            else {
                echo '<li class="page-item"><a class="page-link" href="?page='.$prevpage.'&search='.$search.'">Previous</a></li>';
@@ -146,7 +192,11 @@
             if($i >= $maxpage)
                 break;
             if($exact) {
-                echo '<li class="page-item"><a class="page-link" href="?page='.$i.'&search='.$search.'&exact=true">'.$i.'</a></li>';
+                if($dateStart && $dateEnd) {
+                    echo '<li class="page-item"><a class="page-link" href="?page='.$i.'&search='.$search.'&exact=true&dateStart='.$dateStart.'&dateEnd='.$dateEnd.'">'.$i.'</a></li>';    
+                }
+                else
+                    echo '<li class="page-item"><a class="page-link" href="?page='.$i.'&search='.$search.'&exact=true">'.$i.'</a></li>';
             }
             else {
                 echo '<li class="page-item"><a class="page-link" href="?page='.$i.'&search='.$search.'">'.$i.'</a></li>';                
@@ -155,15 +205,26 @@
        
         if($nextpage <= $pageNum) {
             if($exact) {
-                 echo '<li class="page-item"><a class="page-link" href="?page='.$nextpage.'&search='.$search.'&exact=true">Next</a></li>';
+                if($dateStart && $dateEnd) {
+                    echo '<li class="page-item"><a class="page-link" href="?page='.$nextpage.'&search='.$search.'&exact=true&dateStart='.$dateStart.'&dateEnd='.$dateEnd.'">Next</a></li>';
+                }
+                else
+                    echo '<li class="page-item"><a class="page-link" href="?page='.$nextpage.'&search='.$search.'&exact=true">Next</a></li>';
             }
             else {
                  echo '<li class="page-item"><a class="page-link" href="?page='.$nextpage.'&search='.$search.'">Next</a></li>';
             }
         }
         if($exact) {
-           echo '<li class="page-item">
-              <a class="page-link" href="?page='.$pageNum.'&search='.$search.'&exact=true">Last</a></li>';
+            if($dateStart && $dateEnd) {
+                echo '<li class="page-item">
+                      <a class="page-link" href="?page='.$pageNum.'&search='.$search.'&exact=true&dateStart='.$dateStart.'&dateEnd='.$dateEnd.'">Last</a></li>';
+            }
+            else
+            {
+                echo '<li class="page-item">
+                      <a class="page-link" href="?page='.$pageNum.'&search='.$search.'&exact=true">Last</a></li>';
+            }
         }
         else {
            echo '<li class="page-item">
@@ -175,6 +236,11 @@
      }
     ?>
     <!-- Scripts -->
-    <script src="scripts/search.js"></script> 
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />                                   
+    <script src="scripts/search.js"></script>
   </body>
 </html>
